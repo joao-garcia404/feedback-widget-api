58d52178aeafd172c93295ad59d2b87a86b0904b
Joao Garcia <jvdasilvaeirasgarcia@gmail.com>
2022-05-07 19:54:50 -0300

    Feat: create feedback route and new feedback table

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,22 @@
 import express, { Request, Response } from "express";
+import { prisma } from "./prisma";
 
 const app = express();
 
-app.get("/users", (req: Request, res: Response) => {
-  res.send("users");
+app.use(express.json());
+
+app.post("/feedbacks", async (req: Request, res: Response) => {
+  const { type, comment, screenshot } = req.body;
+
+  const feedback = await prisma.feedback.create({
+    data: {
+      type,
+      comment,
+      screenshot,
+    },
+  });
+
+  return res.status(201).json({ data: feedback });
 });
 
 app.listen(3333, () => {
